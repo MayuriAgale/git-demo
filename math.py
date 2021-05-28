cba9292bc9ad9fb95881527134b2117f1b26e5b9
mayuriagale <mayuagale@gmail.com>
2021-05-28 14:49:51 +0530

    Added function headers

--- math.py
+++ math.py
@@ -1,8 +1,15 @@
+#Add Implimentation
 def add(x,y):
     pass
+
+#Substract Implimentation
 def subtract(x,y):
     pass
+
+#Multiply Implimentation
 def multiply(x,y):
     pass
+
+#Division Implimentation
 def divide(x,y):
     pass
